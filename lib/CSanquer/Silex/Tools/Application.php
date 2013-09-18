a0ae8372f4b40fde43d43eb282db452c2b58c6e5
Charles Sanquer <charles.sanquer-5com@rfi.fr>
2013-09-18 14:24:03 +0200

    add var directory property to Command application

--- lib/CSanquer/Silex/Tools/Application.php
+++ lib/CSanquer/Silex/Tools/Application.php
@@ -16,6 +16,8 @@ class Application extends BaseApplication
     
     protected $appDir;
     
+    protected $varDir;
+    
     protected $configDir;
     
     protected $webDir;
@@ -30,17 +32,18 @@ class Application extends BaseApplication
 
     /**
      * 
-     * @param \Silex\Application $application
-     * @param string $rootDir
-     * @param string $name
-     * @param string $version
-     * @param string $appDir
-     * @param string $configDir
-     * @param string $webDir
-     * @param string $cacheDir
-     * @param string $logsDir
-     * @param string $binDir
-     * @param string $translationDir
+     * @param \Silex\Application $application Silex application
+     * @param string $rootDir project directory path
+     * @param string $name default = 'UNKNOWN'
+     * @param string $version $name default = 'UNKNOWN'
+     * @param string $appDir $name default = '' directory in root directory that contain config , translation, views, php silex bootstrap files
+     * @param string $varDir $name default = 'var' directory in root directory that contain cache and logs
+     * @param string $configDir default = 'config' configuration directory name in app directory
+     * @param string $webDir default = 'web' web document root directory name in root directory
+     * @param string $cacheDir default = 'cache' cache directory name
+     * @param string $logsDir default = 'logs' cache directory name
+     * @param string $binDir default = 'bin' binaries directory name in root directory
+     * @param string $translationDir = default = 'translation' translation directory name in app directory
      */
     public function __construct(
         $application,
@@ -48,6 +51,7 @@ class Application extends BaseApplication
         $name = 'UNKNOWN', 
         $version = 'UNKNOWN',
         $appDir = '',
+        $varDir = 'var',
         $configDir = 'config', 
         $webDir = 'web', 
         $cacheDir = 'cache', 
@@ -59,6 +63,7 @@ class Application extends BaseApplication
         $this->setSilexApplication($application);
         $this->rootDir = realpath($rootDir);
         $this->appDir = $appDir;
+        $this->varDir = $varDir;
         $this->configDir = $configDir;
         $this->webDir = $webDir;
         $this->cacheDir = $cacheDir;
@@ -98,33 +103,38 @@ class Application extends BaseApplication
         return $this->getRootDir().( $this->appDir ? DS.$this->appDir : '');
     }
 
+    public function getVarDir()
+    {
+        return $this->getRootDir().( $this->varDir ? DS.$this->varDir : '');
+    }
+    
     public function getConfigDir()
     {
         return $this->getAppDir().DS.$this->configDir;
     }
 
+    public function getTranslationDir()
+    {
+        return $this->getAppDir().DS.$this->translationDir;
+    }
+    
     public function getWebDir()
     {
         return $this->getRootDir().DS.$this->webDir;
-    }
-
-    public function getCacheDir()
-    {
-        return $this->getAppDir().DS.$this->cacheDir;
-    }
-
-    public function getLogsDir()
-    {
-        return $this->getAppDir().DS.$this->logsDir;
     }
 
     public function getBinDir()
     {
         return $this->getRootDir().DS.$this->binDir;
     }
-
-    public function getTranslationDir()
+    
+    public function getCacheDir()
     {
-        return $this->getAppDir().DS.$this->translationDir;
+        return $this->getVarDir().DS.$this->cacheDir;
+    }
+
+    public function getLogsDir()
+    {
+        return $this->getVarDir().DS.$this->logsDir;
     }
 }
